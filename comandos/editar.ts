b2978f4866ef4a92369b85c6f46b2848a55e7447
Amiko <senorbinario@tuta.io>
2022-05-18 10:27:47 -0600

    Comando para editar frases

---
+++ comandos/editar.ts
@@ -0,0 +1,30 @@
+import Bot from "../types/bot.type";
+import { FIRMA } from "../constants.example";
+import { frasesDB } from "../db/collections/collections";
+import getBotonesFrases from "../acciones/getBotonesFrases";
+
+export default function editar(bot: Bot) {
+  bot.command(["edit", "editar"], (ctx) => {
+    const [, idString, ...palabras] = ctx.message.text.split(" ");
+
+    const id = parseInt(idString);
+    if (isNaN(id)) return ctx.reply(id + " no es un número.");
+
+    const frase = frasesDB.findOne({ $loki: id });
+    if (!frase)
+      return ctx.replyWithHTML(
+        "Ese ID no corresponde a ninguna frase. Los ID válidos son:\n\n<code>" +
+          frasesDB
+            .find({})
+            .map(({ $loki }) => $loki)
+            .join(", ") +
+          "</code>"
+      );
+
+    const nuevaFrase = palabras.join(" ");
+    if (nuevaFrase === "") return ctx.reply("Tienes que decirme una nueva frase luego del ID.");
+
+    frase.frase = nuevaFrase;
+    ctx.reply(frase.frase + FIRMA, getBotonesFrases(frase.$loki, ctx)).catch((e) => console.error(e));
+  });
+}
