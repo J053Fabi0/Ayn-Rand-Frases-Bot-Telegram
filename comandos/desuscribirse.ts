128f07a73d72432ee5c57dd2a00ce38546dd791d
Amiko <senorbinario@tuta.io>
2022-07-06 14:31:13 -0600

    Los comandos de suscripción en los grupos solo funcionan por administradores

--- comandos/desuscribirse.ts
+++ comandos/desuscribirse.ts
@@ -2,13 +2,27 @@ import Bot from "../types/bot.type";
 import { personasDB } from "../db/collections/collections";
 
 export default function desuscribirse(bot: Bot) {
-  bot.command("desuscribirse", (ctx) => {
-    const userID = ctx.message.chat.id;
-    if (personasDB.findOne({ userID })) {
-      personasDB.findAndRemove({ userID });
-      ctx.reply("Listo, no recibirás más frases.");
-    } else {
-      ctx.reply("No estabas suscrito.");
+  bot.command(["desuscribirse", "desuscribir", "desuscribirme"], async (ctx) => {
+    const chatID = ctx.chat.id;
+
+    const admins =
+      ctx.chat.type === "private"
+        ? undefined
+        : (await bot.telegram.getChatAdministrators(chatID)).map(({ user: { id } }) => id);
+
+    // Si estamos en un grupo
+    if (admins) {
+      // Y es administrador
+      if (admins.includes(ctx.message.from.id)) desuscribir(chatID);
+    } else desuscribir(chatID);
+
+    function desuscribir(chatID: number) {
+      if (personasDB.findOne({ userID: chatID })) {
+        personasDB.findAndRemove({ userID: chatID });
+        ctx.reply("Listo, no recibirás más frases.");
+      } else {
+        ctx.reply("No estabas suscrito.");
+      }
     }
   });
 }
