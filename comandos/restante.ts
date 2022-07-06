1744b6f74eb39ae4e1846ec5081a25caa506d9d8
Amiko <senorbinario@tuta.io>
2022-07-06 14:05:29 -0600

    Comando /restante solo funciona en privado

--- comandos/restante.ts
+++ comandos/restante.ts
@@ -3,7 +3,8 @@ import timeUntilHour from "../utils/timeUntilHour";
 import { HORA_DE_PUBLICACIÓN } from "../constants";
 
 export default function restante(bot: Bot) {
-  bot.command("restante", (ctx) =>
-    ctx.reply(`${BigInt(timeUntilHour(HORA_DE_PUBLICACIÓN)) / 1000n / 60n} minutos.`)
-  );
+  bot.command("restante", (ctx) => {
+    if (ctx.chat.type === "private")
+      ctx.reply(`${BigInt(timeUntilHour(HORA_DE_PUBLICACIÓN)) / 1000n / 60n} minutos.`);
+  });
 }
