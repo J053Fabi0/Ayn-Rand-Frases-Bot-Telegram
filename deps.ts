2d1248b8226aed69646aa81427eda60bcd34d233
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-22 18:56:43 -0600

    Setup router

--- deps.ts
+++ deps.ts
@@ -2,3 +2,10 @@ export { sleep } from "sleep";
 export { default as loki } from "lokijs";
 export { lodash as _, lodash } from "lodash";
 export { Bot, InlineKeyboard } from "grammy/mod.ts";
+
+export { opineCors } from "cors";
+export { default as Joi } from "joi";
+export type { ObjectSchema } from "joi";
+
+export { default as opine, json, Router } from "opine";
+export type { NextFunction, OpineResponse, OpineRequest, Params, ParamsDictionary } from "opine";
--- env.ts
+++ env.ts
@@ -1,2 +1,3 @@
+export const PORT = Deno.env.get("PORT") as string;
 export const ADMIN_ID = Deno.env.get("ADMIN_ID") as string;
 export const BOT_TOKEN = Deno.env.get("BOT_TOKEN") as string;
