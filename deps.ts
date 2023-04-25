bc934de763953519b7b7e6832e809daf1ed48c48
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-25 15:51:26 -0600

    Deleted all api-related code

--- deps.ts
+++ deps.ts
@@ -1,17 +1,9 @@
 export { sleep } from "sleep";
-export { opineCors } from "cors";
-export { default as loki } from "lokijs";
 export { lodash as _, lodash } from "lodash";
 
 export type { Filter as FilterCtx } from "grammy/mod.ts";
 export { Bot, InlineKeyboard, Context } from "grammy/mod.ts";
 
-export { default as Joi } from "joi";
-export type { ObjectSchema } from "joi";
-
 export { ObjectId, MongoClient } from "mongo";
 export type { FindAndModifyOptions, InsertDocument, AggregateOptions, AggregatePipeline } from "mongo";
 export type { Filter, Collection, FindOptions, UpdateFilter, UpdateOptions, InsertOptions } from "mongo";
-
-export { default as opine, json, Router } from "opine";
-export type { NextFunction, OpineResponse, OpineRequest, Params, ParamsDictionary } from "opine";
--- index.ts
+++ index.ts
@@ -1,29 +1,10 @@
 // load the .env file
 import "dotenv";
-import router from "./routes/routes.ts";
-import handleError from "./utils/handleError.ts";
-import CommonRequest from "./types/commonRequest.type.ts";
-import CommonResponse from "./types/commonResponse.type.ts";
-import { opine, opineCors, json, NextFunction } from "./deps.ts";
 
 // Check that all required .env variables are set
-const envVariables = ["ADMIN_ID", "BOT_TOKEN", "PORT", "MONGO_URI"];
+const envVariables = ["ADMIN_ID", "BOT_TOKEN", "MONGO_URI"];
 for (const envVariable of envVariables)
   if (Deno.env.get(envVariable) === undefined) console.log(envVariable + " not set in .env."), Deno.exit(0);
 
 // Then start the bot
 import("./initBot.ts");
-
-// Create the web server app
-const app = opine();
-app.use(json());
-app.use(opineCors());
-
-// Use all the routes
-app.use("/", router);
-
-// Error handling
-app.use((err: Error, _: CommonRequest, res: CommonResponse, __: NextFunction) => handleError(res, err));
-
-const port = +Deno.env.get("PORT")!;
-app.listen(port, () => console.log(`Listening on: http://localhost:${port}`));
