ba083601e7c328a29a2eaaeaac13db7c8219d66b
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-24 22:39:08 -0600

    Tackle many bugs

--- utils/tellIDIsNotValid.ts
+++ utils/tellIDIsNotValid.ts
@@ -4,7 +4,7 @@ import { Context, FilterCtx } from "../deps.ts";
 export default async function tellIDIsNotValid(ctx: FilterCtx<Context, "message">) {
   return ctx.reply(
     "Ese ID no corresponde a ninguna frase. Los ID válidos son:\n\n<code>" +
-      (await getAllQuotesNumbers()).join(", ") +
+      (await getAllQuotesNumbers()).sort().join(", ") +
       "</code>",
     { parse_mode: "HTML" }
   );
