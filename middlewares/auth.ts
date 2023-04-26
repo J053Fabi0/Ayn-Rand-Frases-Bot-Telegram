b251977b84c4bf414af3631660a13e32a5e708b3
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-25 20:53:48 -0600

    wait a random ammout of time in auth middleware to prevent brute force attacks

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -3,7 +3,10 @@ import { NextFunction } from "../deps.ts";
 import CommonRequest from "../types/commonRequest.type.ts";
 import CommonResponse from "../types/commonResponse.type.ts";
 
-export default function auth(req: CommonRequest, res: CommonResponse, next: NextFunction) {
+export default async function auth(req: CommonRequest, res: CommonResponse, next: NextFunction) {
+  // wait a random ammout of time to prevent brute force attacks
+  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+
   if (req.headers.get("auth_token") === AUTH_TOKEN) next();
   else res.setStatus(401).send({ message: null, error: "Unauthorized" });
 }
