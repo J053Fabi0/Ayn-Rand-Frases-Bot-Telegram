5249c0bb043e1c22a3a67e5ad2d210c20dd5bfaa
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-25 20:44:49 -0600

    Use croner to schedule the daily publication of quotes

--- commands/suscribirse.ts
+++ commands/suscribirse.ts
@@ -1,10 +1,11 @@
 import { Bot, FilterCtx, Context } from "../deps.ts";
-import timeUntilHour from "../utils/timeUntilHour.ts";
-import { HORA_DE_PUBLICACIÓN } from "../constants.ts";
+import { publishQuoteCron } from "../utils/crons.ts";
+import { PUBLICATION_HOUR, TIMEZONE } from "../constants.ts";
+import msToTimeDescription from "../utils/msToTimeDescription.ts";
 import { createUser, getUser } from "../controllers/mongo/user.controller.ts";
 
 export default function suscribirse(bot: Bot) {
-  bot.command(["suscribirse", "suscribir", "suscribirme"], async (ctx) => {
+  bot.command(["suscribirse", "suscribir", "suscribirme", "subscribe"], async (ctx) => {
     if (!ctx.message) return;
 
     const chatID = ctx.chat.id;
@@ -28,8 +29,9 @@ async function subscribe(ctx: FilterCtx<Context, "message">, chatID: number) {
   } else {
     await createUser({ telegramID: chatID });
     ctx.reply(
-      `Muy bien. A partir de ahora te enviaré frases diario. ` +
-        `La siguiente llegará en ${BigInt(timeUntilHour(HORA_DE_PUBLICACIÓN)) / 1000n / 60n} minutos.`
+      `Muy bien. A partir de ahora te enviaré frases diario.\n\n` +
+        `La siguiente llegará en ${msToTimeDescription(publishQuoteCron.msToNext() || 0)}, ` +
+        `a las ${PUBLICATION_HOUR} horas, tiempo de ${TIMEZONE}.`
     );
   }
 }
