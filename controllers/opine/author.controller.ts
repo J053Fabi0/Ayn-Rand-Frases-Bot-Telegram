2d4339370ef1bd2819341ccac27a1c74854fb62e
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-26 17:17:31 -0600

    Also send the number of quotes when asking for all the authors

--- controllers/opine/author.controller.ts
+++ controllers/opine/author.controller.ts
@@ -1,6 +1,12 @@
+import { countQuotes } from "../mongo/quote.controller.ts";
 import { GetAuthors } from "../../types/api/author.type.ts";
 import CommonResponse from "../../types/commonResponse.type.ts";
 import { getAuthors as getAuthorsCtrl } from "../mongo/author.controller.ts";
 
-export const getAuthors = async (_: GetAuthors, res: CommonResponse) =>
-  res.send({ message: (await getAuthorsCtrl({})).map((a) => ({ id: a._id, name: a.name })) });
+export const getAuthors = async (_: GetAuthors, res: CommonResponse) => {
+  const authors = (await getAuthorsCtrl({})).map((a) => ({ id: a._id, name: a.name, numberOfQuotes: 0 }));
+
+  await Promise.all(authors.map(async (a) => (a.numberOfQuotes = await countQuotes({ author: a.id }))));
+
+  res.send({ message: authors });
+};
