7c5b0cb679288d13965cdbe44472a9a9a07708d0
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-26 16:29:05 -0600

    Less random time between auth request

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -5,7 +5,7 @@ import CommonResponse from "../types/commonResponse.type.ts";
 
 export default async function auth(req: CommonRequest, res: CommonResponse, next: NextFunction) {
   // wait a random ammout of time to prevent brute force attacks
-  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+  await new Promise((resolve) => setTimeout(resolve, Math.random() * 100));
 
   if (req.headers.get("auth_token") === AUTH_TOKEN) next();
   else res.setStatus(401).send({ message: null, error: "Unauthorized" });
