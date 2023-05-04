06165c759f6f4a5723df7feb671bf1f404e19a5e
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-03 19:24:50 -0600

    patch /author

--- routes/author.routes.ts
+++ routes/author.routes.ts
@@ -5,6 +5,9 @@ import * as c from "../controllers/opine/author.controller.ts";
 const authorRoutes = new Router();
 
 authorRoutes.get("/authors", s.getAuthors, c.getAuthors);
+
 authorRoutes.post("/author", s.postAuthor, c.postAuthor);
+
+authorRoutes.patch("/author", s.patchAuthor, c.patchAuthor);
 
 export default authorRoutes;
--- schemas/author.schema.ts
+++ schemas/author.schema.ts
@@ -5,3 +5,5 @@ const name = joi.string().min(3).max(30);
 export const getAuthors = a(joi.object({}), "query");
 
 export const postAuthor = a(joi.object({ name: name.required() }));
+
+export const patchAuthor = a(joi.object({ id: joi.string().required(), name: name.required() }));
