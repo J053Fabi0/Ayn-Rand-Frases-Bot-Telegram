ee8fdf2d526b41a11d7d35367214bd6ecc095ef1
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-03 18:50:33 -0600

    post /quote

--- controllers/opine/quote.controller.ts
+++ controllers/opine/quote.controller.ts
@@ -1,9 +1,10 @@
 import { ObjectId } from "../../deps.ts";
 import { pretifyIds } from "../../utils/pretifyId.ts";
-import { GetQuotes } from "../../types/api/quote.type.ts";
+import { getSourceById } from "../mongo/source.controller.ts";
 import { getAuthorById } from "../mongo/author.controller.ts";
 import CommonResponse from "../../types/commonResponse.type.ts";
-import { getQuotes as getQuotesCtrl } from "../mongo/quote.controller.ts";
+import { GetQuotes, PostQuote } from "../../types/api/quote.type.ts";
+import { getQuotes as getQuotesCtrl, createQuote, aggregateQuote } from "../mongo/quote.controller.ts";
 
 export const getQuotes = async ({ params }: GetQuotes, res: CommonResponse) => {
   const author = await getAuthorById(params.authorId, { projection: { _id: 1 } });
@@ -14,4 +15,28 @@ export const getQuotes = async ({ params }: GetQuotes, res: CommonResponse) => {
   );
 
   res.send({ message: quotes });
+};
+
+export const postQuote = async ({ body }: PostQuote, res: CommonResponse) => {
+  const author = await getAuthorById(body.authorId, { projection: { _id: 1 } });
+  if (!author) res.setStatus(404).send({ message: null, error: "Author not found" });
+
+  if (body.sourceId) {
+    const source = await getSourceById(body.sourceId, { projection: { _id: 1 } });
+    if (!source) res.setStatus(404).send({ message: null, error: "Source not found" });
+  }
+
+  const lastNumber =
+    (await aggregateQuote([{ $group: { _id: null, number: { $max: "$number" } } }]))[0]?.number ?? 0;
+
+  const quote = await createQuote({
+    timesSent: 0,
+    quote: body.quote,
+    number: lastNumber + 1,
+    lastSentTime: new Date(1),
+    author: new ObjectId(body.authorId),
+    source: body.sourceId ? new ObjectId(body.sourceId) : null,
+  });
+
+  res.send({ message: quote._id });
 };
--- types/api/quote.type.ts
+++ types/api/quote.type.ts
@@ -1,3 +1,5 @@
 import CommonRequest from "../commonRequest.type.ts";
 
 export type GetQuotes = CommonRequest<undefined, undefined, { authorId: string }>;
+
+export type PostQuote = CommonRequest<{ quote: string; sourceId: string | null; authorId: string }>;
