798b279e919b9225022aadaf6d443a5832f3435a
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-03 18:34:04 -0600

    post /author

--- schemas/author.schema.ts
+++ schemas/author.schema.ts
@@ -1,3 +1,7 @@
 import { a, joi } from "./schemaUtils.ts";
 
+const name = joi.string().min(3).max(30);
+
 export const getAuthors = a(joi.object({}), "query");
+
+export const postAuthor = a(joi.object({ name: name.required() }));
