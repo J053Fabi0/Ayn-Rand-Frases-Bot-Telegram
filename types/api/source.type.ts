fd2f255ef958e74cb409a3a7249b6231fa7f2bec
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-03 23:13:24 -0600

    patch /source

--- types/api/source.type.ts
+++ types/api/source.type.ts
@@ -3,3 +3,5 @@ import CommonRequest from "../commonRequest.type.ts";
 export type GetSources = CommonRequest<undefined, undefined, { authorId: string }>;
 
 export type PostSource = CommonRequest<{ name: string; authors: string[] }>;
+
+export type PatchSource = CommonRequest<{ name?: string; authors?: string[]; sourceId: string }>;
