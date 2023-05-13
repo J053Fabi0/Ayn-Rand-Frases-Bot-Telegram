2fa3b4448c67b373b8634767e1897f32799be62d
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-13 08:02:09 -0600

    fix: /frase <id> was not working

--- commands/frase.ts
+++ commands/frase.ts
@@ -1,6 +1,4 @@
 import { Bot } from "../deps.ts";
-import { ADMINS_IDS } from "../env.ts";
-import publishQuote from "../publishQuote.ts";
 import tellIDIsNotValid from "../utils/tellIDIsNotValid.ts";
 import getQuotesButtons from "../callbacks/getQuotesButtons.ts";
 import { getFullQuote } from "../controllers/mongo/quote.controller.ts";
@@ -11,13 +9,11 @@ export default function frase(bot: Bot) {
 
     if (/^\/frases/.test(ctx.message.text || "")) return next();
 
-    const chatID = ctx.chat.id;
-    if (ctx.message.text === "/frase" || ADMINS_IDS.includes(chatID))
-      return publishQuote({ chatID, chatType: ctx.chat.type });
-
     // This is called when the user sends /ver <number>
     const number = parseInt(ctx.message.text!.split(" ")[1]);
     if (isNaN(number)) return ctx.reply(number + " no es un número.");
+
+    const chatID = ctx.chat.id;
 
     const { fullQuote } = await getFullQuote({ number });
 
