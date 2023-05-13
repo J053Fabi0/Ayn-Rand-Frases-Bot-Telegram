e5df6871a405119736fa8747f4507cd475889b3d
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-13 08:15:12 -0600

    Removed option to add quotes from the bot
    From now on, it'll only be possible through the API

--- initBot.ts
+++ initBot.ts
@@ -2,11 +2,7 @@ import { Bot } from "./deps.ts";
 import publishQuote from "./publishQuote.ts";
 import commands from "./commands/commands.ts";
 import { ADMINS_IDS, BOT_TOKEN } from "./env.ts";
-import trueLength from "./utils/trueLength.ts";
 import callbacks from "./callbacks/callbacks.ts";
-import { MESSAGE_LENGTH_LIMIT } from "./constants.ts";
-import { getAuthor } from "./controllers/mongo/author.controller.ts";
-import { aggregateQuote, createQuote } from "./controllers/mongo/quote.controller.ts";
 
 const bot = new Bot(BOT_TOKEN);
 
@@ -30,32 +26,5 @@ bot.on("message", async (ctx, next) => {
 
 callbacks(bot);
 commands(bot, "admin");
-
-// Cuando reciba un mensaje mío, será tratado como una nueva frase para añadir.
-bot.on("message", async (ctx) => {
-  if (!ctx.message.text) return;
-
-  const quote = ctx.message.text;
-
-  const messageLength = trueLength(quote);
-  if (messageLength > MESSAGE_LENGTH_LIMIT)
-    return ctx.reply(`Es muy largo. Mide ${messageLength} y el límite son ${MESSAGE_LENGTH_LIMIT}.`);
-
-  const lastNumber =
-    (await aggregateQuote([{ $group: { _id: null, number: { $max: "$number" } } }]))[0]?.number ?? 0;
-
-  const aynRand = (await getAuthor({ name: "Ayn Rand" }))!;
-  const a = await createQuote({
-    quote,
-    timesSent: 0,
-    source: null,
-    author: aynRand._id,
-    number: lastNumber + 1,
-    lastSentTime: new Date(0),
-  });
-  ctx.reply(`Esta frase tiene el ID: ${a.number}. Usa <code>/borrar ${a.number}</code> para borrarla.`, {
-    parse_mode: "HTML",
-  });
-});
 
 bot.start();
