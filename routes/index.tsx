1c39bffbda2e79cd0172aa2c979b8e328203b6be
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-20 12:04:49 -0600

    Init fresh configs

---
+++ routes/index.tsx
@@ -0,0 +1,16 @@
+import { Head } from "../deps.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <a href="https://fresh.deno.dev">
+          <img width="197" height="37" src="https://fresh.deno.dev/fresh-badge-dark.svg" alt="Made with Fresh" />
+        </a>
+      </div>
+    </>
+  );
+}
