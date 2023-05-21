eb7ae862f21e94dba3c43bf5da2aad1da06514f6
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-21 08:01:45 -0600

    Created global middleware and signin method that uses signed cookies

---
+++ routes/signin.tsx
@@ -0,0 +1,52 @@
+import { AUTH_TOKEN, BOT_TOKEN } from "../env.ts";
+import Button from "../components/Button.tsx";
+import { Header } from "../components/Headers.tsx";
+import { Head, Handlers, compare, createSignedCookie } from "../deps.ts";
+
+export const handler: Handlers = {
+  async POST(req) {
+    const form = await req.formData();
+
+    const authToken = form.get("authToken")?.toString();
+    if (!authToken) return new Response("Missing auth token", { status: 401 });
+    if (!(await compare(authToken, AUTH_TOKEN))) return new Response("Unauthorized", { status: 401 });
+
+    const { headers } = await createSignedCookie("authToken", authToken, BOT_TOKEN, { httpOnly: true, path: "/" });
+    headers.set("location", "/");
+
+    const resp = new Response(null, { status: 303, headers });
+    return resp;
+  },
+};
+
+export default function SignIn() {
+  return (
+    <>
+      <Head>
+        <title>Sign in</title>
+      </Head>
+
+      <Header size={4} class="text-2xl">
+        Sign in as an admin
+      </Header>
+
+      <form method="post">
+        <div class="flex flex-col">
+          <input
+            required
+            type="password"
+            name="authToken"
+            placeholder="Auth token"
+            class="mt-2 p-2 border border-gray-300 rounded w-full"
+          />
+        </div>
+
+        <div class="mt-3 flex justify-center items-center">
+          <Button class="mt-2 p-2" type="submit" color="blue">
+            Sign in
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+}
