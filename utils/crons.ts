2a433c1d3d5e949bcf3eae8a28106cfa0d7b6edf
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-22 13:05:19 -0600

    Cron uses repeat until no error

--- utils/crons.ts
+++ utils/crons.ts
@@ -1,5 +1,7 @@
-import { Cron } from "../deps.ts";
 import publishQuote from "../publishQuote.ts";
+import { Cron, repeatUntilNoError } from "../deps.ts";
 import { PUBLICATION_HOUR, TIMEZONE } from "../constants.ts";
 
-export const publishQuoteCron = new Cron(`0 ${PUBLICATION_HOUR} * * *`, { timezone: TIMEZONE }, publishQuote);
+export const publishQuoteCron = new Cron(`0 ${PUBLICATION_HOUR} * * *`, { timezone: TIMEZONE }, () =>
+  repeatUntilNoError(publishQuote, -1, 1)
+);
