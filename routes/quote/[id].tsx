0d1c85f2514543c75c38c2c8751de3f1f68082b1
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-24 10:55:18 -0600

    /quote/[id] works

--- routes/quote/[id].tsx
+++ routes/quote/[id].tsx
@@ -1,5 +1,36 @@
-import { PageProps } from "$fresh/server.ts";
+import { State } from "../../types/state.type.ts";
+import { Handlers, PageProps, ObjectId } from "../../deps.ts";
+import Quote from "../../types/collections/quote.type.ts";
+import { getFullQuote } from "../../controllers/mongo/quote.controller.ts";
+import Header from "../../components/Headers.tsx";
 
-export default function Greet(props: PageProps) {
-  return <div>{props.params.id}</div>;
+interface QuoteProps {
+  quote: string | null;
+}
+
+export const handler: Handlers<QuoteProps, State> = {
+  async GET(_, ctx) {
+    const { id } = ctx.params;
+
+    // The id can be either the quote number or the quote id
+    const quote = await getFullQuote(!isNaN(parseInt(id)) ? { number: parseInt(id) } : { _id: new ObjectId(id) });
+
+    return await ctx.render({ quote: quote.fullQuote });
+  },
+};
+
+export default function Quote(props: PageProps<QuoteProps>) {
+  const { quote } = props.data;
+
+  if (!quote) return <Header size={4}>Quote not found</Header>;
+
+  const splitQuote = quote.split("\n");
+
+  return (
+    <p>
+      {splitQuote.map((t, i) => (
+        <p class={`mt-2${i === splitQuote.length - 1 && /^ - /.test(t) ? " ml-2" : ""}`}>{t}</p>
+      ))}
+    </p>
+  );
 }
