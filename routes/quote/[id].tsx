a9c7d53c1c7186a5ca691c08a9f6adecc2f5d249
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-24 23:40:50 -0600

    Show the author and source at the bottom

--- routes/quote/[id].tsx
+++ routes/quote/[id].tsx
@@ -6,8 +6,7 @@ import { Handlers, Head, PageProps, ObjectId } from "../../deps.ts";
 import { PossibleQuote, getFullQuote } from "../../controllers/mongo/quote.controller.ts";
 
 interface QuoteProps {
-  fullQuote: string | null;
-  quote: PossibleQuote | null;
+  quoteObj: PossibleQuote | null;
 }
 
 export const handler: Handlers<QuoteProps, State> = {
@@ -17,14 +16,14 @@ export const handler: Handlers<QuoteProps, State> = {
     // The id can be either the quote number or the quote id
     const quote = await getFullQuote(!isNaN(parseInt(id)) ? { number: parseInt(id) } : { _id: new ObjectId(id) });
 
-    return await ctx.render({ fullQuote: quote.fullQuote, quote: quote.possibleQuote });
+    return await ctx.render({ quoteObj: quote.possibleQuote });
   },
 };
 
 export default function Quote(props: PageProps<QuoteProps>) {
-  const { fullQuote, quote } = props.data;
+  const { quoteObj } = props.data;
 
-  if (!fullQuote || !quote)
+  if (!quoteObj)
     return (
       <>
         <Head>
@@ -34,18 +33,25 @@ export default function Quote(props: PageProps<QuoteProps>) {
       </>
     );
 
-  const splitQuote = fullQuote.split("\n");
-  const description = quote.quote.replace(/\n/g, " ").slice(0, 50) + (quote.quote.length > 50 ? "…" : "");
+  const splitQuote = quoteObj.quote.split("\n");
+  const author = quoteObj.author[0]?.name || "Unknown";
+  const source = quoteObj.source[0]?.name || "";
+  const quote = quoteObj.quote.replace(/\n/g, " ");
+
+  const description = quote.slice(0, 50) + (quote.length > 50 ? "…" : "");
 
   return (
     <>
       <Head>
-        <Metas description={description} title={`Quote from ${quote.author[0]?.name}`} />
+        <Metas description={description} title={`Quote from ${author}`} />
       </Head>
       <p>
         {splitQuote.map((t, i) => (
-          <p class={`mt-2${i === splitQuote.length - 1 && /^ - /.test(t) ? " ml-2" : ""}`}>{t}</p>
+          <p class={i > 0 ? "mt-2" : ""}>{t}</p>
         ))}
+        <p class="mt-4 ml-2 font-semibold">
+          - {author}. <i>{source}</i>.
+        </p>
       </p>
     </>
   );
