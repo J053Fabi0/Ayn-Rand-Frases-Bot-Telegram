cfaf41e03af1269fcd2aa91675c2834d78c766ce
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-25 10:23:09 -0600

    author/new to publish new authors

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,22 +5,24 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_app.tsx";
 import * as $1 from "./routes/_middleware.tsx";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/quote/[id].tsx";
-import * as $4 from "./routes/quote/new.tsx";
-import * as $5 from "./routes/signin.tsx";
-import * as $6 from "./routes/source/new.tsx";
+import * as $2 from "./routes/author/new.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/quote/[id].tsx";
+import * as $5 from "./routes/quote/new.tsx";
+import * as $6 from "./routes/signin.tsx";
+import * as $7 from "./routes/source/new.tsx";
 import * as $$0 from "./islands/AuthorSourceSelector.tsx";
 
 const manifest = {
   routes: {
     "./routes/_app.tsx": $0,
     "./routes/_middleware.tsx": $1,
-    "./routes/index.tsx": $2,
-    "./routes/quote/[id].tsx": $3,
-    "./routes/quote/new.tsx": $4,
-    "./routes/signin.tsx": $5,
-    "./routes/source/new.tsx": $6,
+    "./routes/author/new.tsx": $2,
+    "./routes/index.tsx": $3,
+    "./routes/quote/[id].tsx": $4,
+    "./routes/quote/new.tsx": $5,
+    "./routes/signin.tsx": $6,
+    "./routes/source/new.tsx": $7,
   },
   islands: {
     "./islands/AuthorSourceSelector.tsx": $$0,
