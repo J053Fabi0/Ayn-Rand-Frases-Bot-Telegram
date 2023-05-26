53501a26e62360f445ef15f3667a64b3d0a75bd7
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-25 22:52:36 -0600

    Added favicon with new Links component inside _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,22 +1,34 @@
-import { AppProps } from "../deps.ts";
+import { AppProps, Head } from "../deps.ts";
 import Navbar from "../components/Navbar.tsx";
+import { Links } from "../components/Links.tsx";
 
 export default function App({ Component }: AppProps) {
   return (
-    <body class="min-h-screen flex flex-col">
-      <Navbar />
+    <>
+      <Head>
+        <Links />
+      </Head>
 
-      <div class="px-4 pt-8 mx-auto w-full max-w-screen-md flex-1">
-        <Component />
-      </div>
+      <body class="min-h-screen flex flex-col">
+        <Navbar />
 
-      <div class="mt-6 py-5 bg-gray-300 text-white">
-        <div class="px-4 mx-auto w-full max-w-screen-lg flex justify-center items-center gap-2">
-          <a href="https://fresh.deno.dev">
-            <img width="197" height="37" src="https://fresh.deno.dev/fresh-badge-dark.svg" alt="Made with Fresh" />
-          </a>
+        <div class="px-4 pt-8 mx-auto w-full max-w-screen-md flex-1">
+          <Component />
         </div>
-      </div>
-    </body>
+
+        <div class="mt-6 py-5 bg-gray-300 text-white">
+          <div class="px-4 mx-auto w-full max-w-screen-lg flex justify-center items-center gap-2">
+            <a href="https://fresh.deno.dev">
+              <img
+                width="197"
+                height="37"
+                src="https://fresh.deno.dev/fresh-badge-dark.svg"
+                alt="Made with Fresh"
+              />
+            </a>
+          </div>
+        </div>
+      </body>
+    </>
   );
 }
