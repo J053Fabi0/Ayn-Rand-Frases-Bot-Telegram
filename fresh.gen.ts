d84b9a356dc819b10a567c4c9b45fc31b427d5ed
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-27 14:56:00 -0600

    404 page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,28 +3,30 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/_app.tsx";
-import * as $1 from "./routes/_middleware.tsx";
-import * as $2 from "./routes/author/new.tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/quote/[id].tsx";
-import * as $5 from "./routes/quote/new.tsx";
-import * as $6 from "./routes/signin.tsx";
-import * as $7 from "./routes/signout.tsx";
-import * as $8 from "./routes/source/new.tsx";
+import * as $0 from "./routes/_404.tsx";
+import * as $1 from "./routes/_app.tsx";
+import * as $2 from "./routes/_middleware.tsx";
+import * as $3 from "./routes/author/new.tsx";
+import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/quote/[id].tsx";
+import * as $6 from "./routes/quote/new.tsx";
+import * as $7 from "./routes/signin.tsx";
+import * as $8 from "./routes/signout.tsx";
+import * as $9 from "./routes/source/new.tsx";
 import * as $$0 from "./islands/AuthorSourceSelector.tsx";
 
 const manifest = {
   routes: {
-    "./routes/_app.tsx": $0,
-    "./routes/_middleware.tsx": $1,
-    "./routes/author/new.tsx": $2,
-    "./routes/index.tsx": $3,
-    "./routes/quote/[id].tsx": $4,
-    "./routes/quote/new.tsx": $5,
-    "./routes/signin.tsx": $6,
-    "./routes/signout.tsx": $7,
-    "./routes/source/new.tsx": $8,
+    "./routes/_404.tsx": $0,
+    "./routes/_app.tsx": $1,
+    "./routes/_middleware.tsx": $2,
+    "./routes/author/new.tsx": $3,
+    "./routes/index.tsx": $4,
+    "./routes/quote/[id].tsx": $5,
+    "./routes/quote/new.tsx": $6,
+    "./routes/signin.tsx": $7,
+    "./routes/signout.tsx": $8,
+    "./routes/source/new.tsx": $9,
   },
   islands: {
     "./islands/AuthorSourceSelector.tsx": $$0,
