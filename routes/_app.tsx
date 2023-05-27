96b2378de425b8ca781a208dfd0b21ea5690a5a6
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-26 22:02:46 -0600

    Fresh svg is now served locally and as an asset

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps, Head } from "../deps.ts";
 import Navbar from "../components/Navbar.tsx";
 import { Links } from "../components/Links.tsx";
+import { AppProps, Head, asset } from "../deps.ts";
 
 export default function App({ Component }: AppProps) {
   return (
@@ -19,12 +19,7 @@ export default function App({ Component }: AppProps) {
         <div class="mt-6 py-5 bg-gray-300 text-white">
           <div class="px-4 mx-auto w-full max-w-screen-lg flex justify-center items-center gap-2">
             <a href="https://fresh.deno.dev">
-              <img
-                width="197"
-                height="37"
-                src="https://fresh.deno.dev/fresh-badge-dark.svg"
-                alt="Made with Fresh"
-              />
+              <img width="197" height="37" src={asset("/fresh-badge-dark.svg")} alt="Made with Fresh" />
             </a>
           </div>
         </div>
