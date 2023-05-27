033b026ff97cda3d9d0b09e93fbf6c28378196ff
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-26 19:36:58 -0600

    Paginator buttons inside /quote/[id]

--- routes/quote/[id].tsx
+++ routes/quote/[id].tsx
@@ -1,13 +1,18 @@
+import Button from "../../components/Button.tsx";
 import isMongoId from "../../utils/isMongoId.ts";
 import { State } from "../../types/state.type.ts";
 import { Metas } from "../../components/Metas.tsx";
 import Typography from "../../components/Typography.tsx";
 import Quote from "../../types/collections/quote.type.ts";
 import { Handlers, Head, PageProps, ObjectId } from "../../deps.ts";
-import { FullQuote, getFullQuote } from "../../controllers/mongo/quote.controller.ts";
+import CaretLeftFill from "../../components/Icons/CaretLeftFill.tsx";
+import CaretRightFill from "../../components/Icons/CaretRightFill.tsx";
+import { FullQuote, getFullQuote, getFullQuotes } from "../../controllers/mongo/quote.controller.ts";
 
 interface QuoteProps {
   quoteObj: FullQuote | null;
+  next: number;
+  previous: number;
 }
 
 export const handler: Handlers<QuoteProps, State> = {
@@ -17,12 +22,21 @@ export const handler: Handlers<QuoteProps, State> = {
     // The id can be either the quote number or the quote id
     const possibleQuote = await getFullQuote(isMongoId(id) ? { _id: new ObjectId(id) } : { number: parseInt(id) });
 
-    return await ctx.render({ quoteObj: possibleQuote });
+    if (possibleQuote) {
+      const fullQuotes = (await getFullQuotes(undefined, { projection: { number: 1 } })).map((q) => q.number);
+      const index = fullQuotes.indexOf(possibleQuote.number);
+      const next = index > 0 ? fullQuotes[index - 1] : fullQuotes[fullQuotes.length - 1];
+      const previous = index < fullQuotes.length - 1 ? fullQuotes[index + 1] : fullQuotes[0];
+
+      return await ctx.render({ quoteObj: possibleQuote, next, previous });
+    }
+
+    return await ctx.render({ quoteObj: possibleQuote, next: 1, previous: 1 });
   },
 };
 
-export default function Quote(props: PageProps<QuoteProps>) {
-  const { quoteObj } = props.data;
+export default function Quote({ data }: PageProps<QuoteProps>) {
+  const { quoteObj } = data;
 
   if (!quoteObj)
     return (
@@ -65,6 +79,21 @@ export default function Quote(props: PageProps<QuoteProps>) {
             )}
           </Typography>
         </div>
+      </div>
+
+      <div class="flex justify-center mt-9">
+        <a href={`/quote/${data.previous}`} alt={`Quote #${data.previous}`}>
+          <Button class="mr-3 flex" color="green">
+            <CaretLeftFill />#{data.previous}
+          </Button>
+        </a>
+
+        <a href={`/quote/${data.next}`} alt={`Quote #${data.next}`}>
+          <Button color="green" class="flex">
+            #{data.next}
+            <CaretRightFill />
+          </Button>
+        </a>
       </div>
     </>
   );
