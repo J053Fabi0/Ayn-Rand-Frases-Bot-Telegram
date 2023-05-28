821013b68e51f6742e2a96108385164416a92dc7
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-28 14:43:45 -0600

    Don't sort the quote numbers

--- utils/tellIDIsNotValid.ts
+++ utils/tellIDIsNotValid.ts
@@ -1,10 +1,10 @@
-import { getAllQuotesNumbers } from "../controllers/mongo/quote.controller.ts";
 import { Context, FilterCtx } from "../deps.ts";
+import { getAllQuotesNumbers } from "../controllers/mongo/quote.controller.ts";
 
 export default async function tellIDIsNotValid(ctx: FilterCtx<Context, "message">) {
   return ctx.reply(
     "Ese ID no corresponde a ninguna frase. Los ID válidos son:\n\n<code>" +
-      (await getAllQuotesNumbers()).sort().join(", ") +
+      (await getAllQuotesNumbers()).join(", ") +
       "</code>",
     { parse_mode: "HTML" }
   );
