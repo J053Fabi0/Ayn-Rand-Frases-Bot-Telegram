e96ec0aaf3c4eb5292e3e2737a049fd23fb2d306
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-31 21:35:29 -0600

    Removed unused console.log

--- utils/getActionAndId.ts
+++ utils/getActionAndId.ts
@@ -8,7 +8,6 @@ export default function getActionAndId<Data, State>(
   urlPattern: URLPattern = defaultUrlPattern
 ) {
   const groups = urlPattern.exec(req.url)!.pathname.groups as { action: "edit" | "new"; id?: string };
-  console.log(groups);
 
   // only allow /quote/new and /quote/edit/:id
   if (groups.action === "edit" && !groups.id) return ctx.renderNotFound();
