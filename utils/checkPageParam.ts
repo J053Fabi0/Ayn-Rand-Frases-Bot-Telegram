4ac654d6118bca35c356acc751d73f07c79e1ff5
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-31 20:06:24 -0600

    Function to checkPageParam

---
+++ utils/checkPageParam.ts
@@ -0,0 +1,16 @@
+import redirect from "./redirect.ts";
+
+export default function checkPageParam(baseUrl: string, queryParams: { page?: string }, pages: number[]) {
+  // If the page is not a number, remove it from the query params
+  if (queryParams.page && isNaN(+queryParams.page)) return redirect(`/${baseUrl}`);
+
+  const page = queryParams.page ? +queryParams.page : 1;
+
+  // If the page is 1 or less, remove it from the query params, as 1 it is the default and 0 or less is not valid
+  if (page <= 1) return redirect(`/${baseUrl}`);
+
+  // If the page is not present in the pages array, redirect to the greatest page
+  if (!pages.includes(page)) return redirect(`/${baseUrl}?page=${pages[pages.length - 1]}`);
+
+  return page;
+}
