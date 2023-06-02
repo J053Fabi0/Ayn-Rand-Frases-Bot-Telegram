9b5f67dc40360d2e0d869ef02eb87542d5cd38b0
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-02 11:37:27 -0600

    Remove the use of a variable constants.ts

--- env.ts
+++ env.ts
@@ -3,9 +3,11 @@ import { hash } from "./deps.ts";
 import "dotenv";
 export const WEB_PORT = +Deno.env.get("WEB_PORT")!;
 export const API_PORT = +Deno.env.get("API_PORT")!;
+export const TIMEZONE = Deno.env.get("TIMEZONE") as string;
 export const BOT_TOKEN = Deno.env.get("BOT_TOKEN") as string;
 export const MONGO_URI = Deno.env.get("MONGO_URI") as string;
 export const WEBSITE_URL = Deno.env.get("WEBSITE_URL") as string;
+export const PUBLICATION_HOUR = +Deno.env.get("PUBLICATION_HOUR")!;
 export const ADMINS_IDS = Deno.env.get("ADMINS_IDS")!.split(",").map(parseInt);
 
 export const AUTH_TOKEN =
