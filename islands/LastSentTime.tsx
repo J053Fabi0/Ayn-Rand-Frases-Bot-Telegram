a83acd54671d2b84a541dd5d939aac360c337ce8
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-13 17:24:29 -0600

    Don't show the last sent time if it's larger than 2 years

--- islands/LastSentTime.tsx
+++ islands/LastSentTime.tsx
@@ -9,12 +9,15 @@ interface LastSentTimeProps {
 export default function LastSentTime({ dateToParse }: LastSentTimeProps) {
   const date = new Date(dateToParse);
 
+  // if date is more than 2 years, return null
+  if (date.getFullYear() < new Date().getFullYear() - 2) return null;
+
   const dayjsDate = dayjs(date);
 
   return (
     <>
       {date.toLocaleString()}
-      <br />
+      &nbsp; - &nbsp;
       {dayjsDate.fromNow()}
     </>
   );
