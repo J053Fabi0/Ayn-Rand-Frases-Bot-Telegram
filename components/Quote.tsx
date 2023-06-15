4ec06112265cdfc36502a2b6a5afb3bfc2f01380
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-14 19:59:22 -0600

    Format the source in the quote view with an url if it has one

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,18 +1,19 @@
+import { createElement, Fragment } from "preact";
 import Typography from "./Typography.tsx";
 import Source from "../types/collections/source.type.ts";
 import Author from "../types/collections/author.type.ts";
 import { FullQuote } from "../controllers/mongo/quote.controller.ts";
 
 export type EssentialQuote = Pick<FullQuote, "quote" | "sourceDetails"> & {
-  source?: { name: Source["name"] } | null;
   author?: { name: Author["name"] } | null;
+  source?: { name: Source["name"]; url?: Source["url"] } | null;
 };
 
 export default function Quote({ quote }: { quote: EssentialQuote }) {
   if (!quote.quote) return null;
 
   const splitQuote = quote.quote.split("\n");
-  const source = (quote.source?.name || "") + (quote.sourceDetails ?? "");
+  const source = quote.source?.name || "";
   const author = quote.author?.name || "Unknown";
 
   return (
@@ -26,10 +27,19 @@ export default function Quote({ quote }: { quote: EssentialQuote }) {
       <Typography variant="lead" class="mt-4 ml-2">
         - {author}.
         {source && (
-          <>
+          <span class="text-lg">
             {" "}
-            <i>{source}</i>.
-          </>
+            {createElement(
+              quote.source?.url ? "a" : (Fragment as unknown as string),
+              {
+                target: "_blank",
+                href: quote.source?.url || "",
+                class: quote.source?.url ? "ml-1 underline" : "",
+              },
+              <i>{source}</i>
+            )}
+            {quote.sourceDetails && <i>{quote.sourceDetails}</i>}.
+          </span>
         )}
       </Typography>
     </>
