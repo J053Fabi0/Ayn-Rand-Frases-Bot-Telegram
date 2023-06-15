92840b8a1c9889a3f789823523bb3bf970148fdc
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-15 14:43:27 -0600

    Import "preact/debug"

--- dev.ts
+++ dev.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S deno run --watch --allow-sys --allow-net --allow-read --allow-env --allow-run --allow-write
 
+import "preact/debug";
 import { dev } from "./deps.ts";
 
 await dev(import.meta.url, "./main.ts");
