53a3633aa59d58c9ca015cc06c528fc4221dac6d
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-11-28 22:42:25 -0600

    Config for builds to work correctly

--- dev.ts
+++ dev.ts
@@ -2,5 +2,6 @@
 
 import "preact/debug";
 import { dev } from "./deps.ts";
+import config from "./fresh.config.ts";
 
-await dev(import.meta.url, "./main.ts");
+await dev(import.meta.url, "./main.ts", config);
--- main.ts
+++ main.ts
@@ -7,12 +7,12 @@
 
 // load the .env file
 import "./utils/crons.ts";
+import { start } from "./deps.ts";
 import manifest from "./fresh.gen.ts";
-import twindConfig from "./twind.config.ts";
-import { start, twindPlugin } from "./deps.ts";
+import config from "./fresh.config.ts";
 
 // Then start the bot
 import("./initBot.ts");
 
 // Start the web server
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, config);
