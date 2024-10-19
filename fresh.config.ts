e6ae592fe63723734aa66139b3617ec636f41d42
Jose Fabio <J053Fabi0@users.noreply.github.com>
2024-10-19 10:30:32 -0600

    Fix support for deno 2.0

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,6 @@
+import twindConfig from "./twind.config.ts";
 import { defineConfig } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
-import twindConfig from "./twind.config.ts";
 
 export default defineConfig({
   plugins: [twindPlugin(twindConfig)],
